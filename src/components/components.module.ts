ac17d9baee7df6d25267a8b5aa15a812c8305fc7
ngxing <409472062@qq.com>
2018-03-12 16:51:14 +0800

    城市搜索/城市代码翻译 API BASE 待优化

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { InitComponent } from './init/init';
-@NgModule({
-	declarations: [InitComponent],
-	imports: [],
-	exports: [InitComponent]
-})
-export class ComponentsModule {}
--- src/pipes/pipes.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { TestPipe } from './test/test';
-@NgModule({
-	declarations: [TestPipe],
-	imports: [],
-	exports: [TestPipe]
-})
-export class PipesModule {}
